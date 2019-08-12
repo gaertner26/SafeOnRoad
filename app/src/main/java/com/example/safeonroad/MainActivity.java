c9a2d92f961cd5161982723937de8c60f2df0c34
Turbochris2000 <christopht33@gmail.com>
2019-08-12 12:27:39 +0200

    Added Timer Variables

--- app/src/main/java/com/example/safeonroad/MainActivity.java
+++ app/src/main/java/com/example/safeonroad/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity{
     private final int PERMISSION_ALREADY_REVOKED = -1;
 
 
+    //Times for Cooldowns etc
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/safeonroad/MainService.java
+++ app/src/main/java/com/example/safeonroad/MainService.java
@@ -3,16 +3,28 @@ package com.example.safeonroad;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.SystemClock;
+
+import java.util.Calendar;
 
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
+
+// Christoph, 2019-8-12
 public class MainService extends Service {
     public boolean isServiceActive = TRUE;
 
+    private long manualCoolddownStartTime = 0;   //timestamp when user told the app to go to standby for 1 hr
+    private long autoCooldownStartTime = 0; // timestamp when the car started to drive < 20 hm/h
 
-    // Christoph, 2019-8-12
+    private int manualCooldownDuration = 60 * 60 * 1000;   //example: 1 hr
+    private int autoCooldownDuration = 3 * 60 * 1000; //3 mins the car has to be driving < 20 km/h
+
+
     private int idBluethoothCar;
+
+
     public MainService() {
         //if this service is active, he should perform every 250 ms the following action:
         while(isServiceActive){
@@ -20,15 +32,16 @@ public class MainService extends Service {
             if(isBluetoothActive() && isBluetoothCarActive()){
                 dontDisturb(TRUE);
             }else{
-                if(currentTime - TimeCooldownStarted >= cooldownDuration){  //check, if app has been paused for 2 hours
+                long time = System.currentTimeMillis();
+                if(System.currentTimeMillis() - manualCoolddownStartTime >= manualCooldownDuration){  //check, if app has been paused for 2 hours
                     int velocity = getVelocity(); //thread 2? idk
-                    if(velocity >= 20km/h  AND getDontDisturb() == false){
+                    if(velocity >= 20 && getDontDisturb() == false){
                         dontDisturb(TRUE);
                         autoCooldownStartTime = 0;
                     }else if(velocity < 20){
                         if(autoCooldownStartTime == 0){   // check, if the car has been at least 2 mins slower than 20 km/h
-                            autoCooldownStartTime = currentTume();
-                        }else if(currentTime - autoCooldownStartTime > 2 minutes){
+                            autoCooldownStartTime = System.currentTimeMillis();
+                        }else if(System.currentTimeMillis() - autoCooldownStartTime > autoCooldownDuration){
                             dontDisturb(FALSE);
                         }
                     }
@@ -50,7 +63,6 @@ public class MainService extends Service {
     }
 
     private void dontDisturb(boolean state){
-
         if(getDontDisturb() == false && state == true){
             setDontDisturb = true;
         }else if(getDontDisturb() == true && state == false){
@@ -69,12 +81,24 @@ public class MainService extends Service {
         return TRUE;
     }
 
-    public void setIdBluethoothCar(int id){
-        idBluethoothCar = id;
+    public void setServiceActive(boolean serviceActive) {
+        isServiceActive = serviceActive;
     }
 
-    public void setActiveState(boolean state){
-        isServiceActive = state;
+    public void setIdBluethoothCar(int idBluethoothCar) {
+        this.idBluethoothCar = idBluethoothCar;
+    }
+
+    public void setManualCoolddownStartTime(long manualCoolddownStartTime) {
+        this.manualCoolddownStartTime = manualCoolddownStartTime;
+    }
+
+    public void setManualCooldownDuration(int manualCooldownDuration) {
+        this.manualCooldownDuration = manualCooldownDuration;
+    }
+
+    public void setAutoCooldownDuration(int autoCooldownDuration) {
+        this.autoCooldownDuration = autoCooldownDuration;
     }
 
 }
